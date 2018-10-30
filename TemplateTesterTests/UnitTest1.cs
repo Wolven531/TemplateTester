b85afaa2141a80eefab8c7e8e66ce759ee7a780b
Anthony Williams <anthony.williams.cs@gmail.com>
2018-10-29 18:45:03 -0600

    Add testing project

---
+++ TemplateTesterTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.TestHost;
+using System;
+using Xunit;
+
+namespace TemplateTesterTests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+			//var client = TestServer.
+		}
+	}
+}
