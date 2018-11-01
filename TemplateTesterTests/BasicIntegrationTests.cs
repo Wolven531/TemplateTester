d7724863fc9069de0044b0fb8089d70a1d2d7d6b
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-01 14:12:30 -0600

    Rename unit tests for clarity

--- TemplateTesterTests/BasicIntegrationTests.cs
+++ TemplateTesterTests/BasicIntegrationTests.cs
@@ -1,22 +1,32 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using System;
+using System.Threading.Tasks;
 using TemplateTester;
 using Xunit;
 
 namespace TemplateTesterTests
 {
-	public class UnitTest1
+	public class BasicIntegrationTests
 	{
 		[Fact]
-		public void Test1()
+		public async Task GetHomePage_WhenInvoked_ShouldReturnHomepage()
 		{
+			// Arrange
 			//var builder = WebHostBuilderFactory.CreateFromAssemblyEntryPoint
 			var server = new TestServer(
 				new WebHostBuilder()
 					.UseEnvironment(EnvironmentName.Staging)
 					.UseStartup<Startup>()
 			);
+			server.BaseAddress = new Uri("https://localhost:5310");
+			var client = server.CreateClient();
+
+			// Act
+			var response = await client.GetAsync("/");
+
+			// Assert
+			response.EnsureSuccessStatusCode();
 		}
 	}
 }
