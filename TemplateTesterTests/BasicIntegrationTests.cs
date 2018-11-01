782d664210d95264e6ec0b498d4fecdd0b4ae622
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-01 14:43:40 -0600

    Structure basic integration tests and get them passing

--- TemplateTesterTests/BasicIntegrationTests.cs
+++ TemplateTesterTests/BasicIntegrationTests.cs
@@ -1,6 +1,8 @@
+using FluentAssertions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using System;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using TemplateTester;
 using Xunit;
@@ -9,24 +11,45 @@ namespace TemplateTesterTests
 {
 	public class BasicIntegrationTests
 	{
+		private const string _ContentTypeCharSet = "utf-8";
+		private const string _ServerURL = "https://localhost:5310";
+
+		private readonly MediaTypeHeaderValue _HTMLContentType;
+		private readonly MediaTypeHeaderValue _JSONContentType;
+		private readonly MediaTypeHeaderValue _TextContentType;
+
+		private readonly TestServer _Server;
+
+		public BasicIntegrationTests()
+		{
+			_HTMLContentType = MediaTypeHeaderValue.Parse("text/html");
+			_HTMLContentType.CharSet = _ContentTypeCharSet;
+			_JSONContentType = MediaTypeHeaderValue.Parse("application/json");
+			_JSONContentType.CharSet = _ContentTypeCharSet;
+			_TextContentType = MediaTypeHeaderValue.Parse("text/plain");
+			_TextContentType.CharSet = _ContentTypeCharSet;
+
+			_Server = new TestServer(
+				new WebHostBuilder()
+					.UseEnvironment(EnvironmentName.Staging)
+					.UseStartup<Startup>())
+			{
+				BaseAddress = new Uri(_ServerURL)
+			};
+		}
+
 		[Fact]
 		public async Task GetHomePage_WhenInvoked_ShouldReturnHomepage()
 		{
 			// Arrange
-			//var builder = WebHostBuilderFactory.CreateFromAssemblyEntryPoint
-			var server = new TestServer(
-				new WebHostBuilder()
-					.UseEnvironment(EnvironmentName.Staging)
-					.UseStartup<Startup>()
-			);
-			server.BaseAddress = new Uri("https://localhost:5310");
-			var client = server.CreateClient();
+			var client = _Server.CreateClient();
 
 			// Act
 			var response = await client.GetAsync("/api");
 
 			// Assert
 			response.EnsureSuccessStatusCode();
+			response.Content.Headers.ContentType.Should().Be(_JSONContentType);
 		}
 	}
 }
