f22b899a66aaf9685d1ec6cdd06de0101b8f25b1
Anthony Williams <anthony.williams.cs@gmail.com>
2018-10-31 20:58:15 -0600

    Light reformat to start clearing bootstrap dep

--- TemplateTester/ClientApp/src/components/NavMenu.js
+++ TemplateTester/ClientApp/src/components/NavMenu.js
@@ -1,35 +1,14 @@
-﻿import React from 'react';
-import { Link } from 'react-router-dom';
-import { Glyphicon, Nav, Navbar, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import './NavMenu.css';
+﻿import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './NavMenu.css'
 
 export default props => (
-  <Navbar inverse fixedTop fluid collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <Link to={'/'}>TemplateTester</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <LinkContainer to={'/'} exact>
-          <NavItem>
-            <Glyphicon glyph='home' /> Home
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/counter'}>
-          <NavItem>
-            <Glyphicon glyph='education' /> Counter
-          </NavItem>
-        </LinkContainer>
-        <LinkContainer to={'/fetchdata'}>
-          <NavItem>
-            <Glyphicon glyph='th-list' /> Fetch data
-          </NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+	<nav>
+		<ul>
+			<li><Link to={'/'}>TemplateTester</Link></li>
+			<li><Link to={'/counter'}>Counter</Link></li>
+			<li><Link to={'/fetchdata'}>Fetch Data</Link></li>
+		</ul>
+	</nav>
+)
