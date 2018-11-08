6a0b96ec197303b0d24f114709a524384732e500
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-07 19:31:26 -0700

    Simpligy POST endpoints in API Home Controller

--- TemplateTester/Controllers/HomeController.cs
+++ TemplateTester/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TemplateTester.Models;
 
 namespace TemplateTester.Controllers
 {
@@ -25,22 +26,35 @@ namespace TemplateTester.Controllers
 			return "value";
 		}
 
-		// POST: api/Home
+		// POST: api
 		[HttpPost]
-		public void Post([FromBody] string value)
+		public ActionResult Post()
 		{
+			return NoContent();
 		}
 
-		// PUT: api/Home/5
-		[HttpPut("{id}")]
-		public void Put(int id, [FromBody] string value)
-		{
-		}
+		//// POST: api
+		//[HttpPost]
+		//public void Post([FromBody] string valueString)
+		//{
+		//}
 
-		// DELETE: api/ApiWithActions/5
-		[HttpDelete("{id}")]
-		public void Delete(int id)
-		{
-		}
+		//// POST: api
+		//[HttpPost]
+		//public void Post([FromBody] JsonContent valueJson)
+		//{
+		//}
+
+		//// PUT: api/Home/5
+		//[HttpPut("{id}")]
+		//public void Put(int id, [FromBody] string value)
+		//{
+		//}
+
+		//// DELETE: api/ApiWithActions/5
+		//[HttpDelete("{id}")]
+		//public void Delete(int id)
+		//{
+		//}
 	}
 }
