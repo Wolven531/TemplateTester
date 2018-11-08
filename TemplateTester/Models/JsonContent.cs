e86c83df1962403095c6333c3682d18cceb886d4
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-07 19:30:48 -0700

    Add JsonContent model class

---
+++ TemplateTester/Models/JsonContent.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace TemplateTester.Models
+{
+	public class JsonContent : StringContent
+	{
+		private const string _ContentTypeCharSet = "utf-8";
+		public static MediaTypeHeaderValue JSONContentType = new MediaTypeHeaderValue("application/json") { CharSet = _ContentTypeCharSet };
+
+		public JsonContent() : base(string.Empty)
+		{
+			Headers.ContentType = JSONContentType;
+		}
+
+		public JsonContent(string value) : base(value)
+		{
+			Headers.ContentType = JSONContentType;
+		}
+
+		public JsonContent(JToken value) : base(value.ToString(Formatting.None))
+		{
+			Headers.ContentType = JSONContentType;
+		}
+	}
+}
