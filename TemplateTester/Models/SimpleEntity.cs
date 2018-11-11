be4370bdfd71c83f4efee13770e93ac4f85e5e30
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-11 11:45:35 -0700

    Add SimpleEntity model for new POST endpoint

---
+++ TemplateTester/Models/SimpleEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TemplateTester.Models
+{
+	public class SimpleEntity
+	{
+		public SimpleEntity(string readableName)
+		{
+			ReadableName = readableName;
+		}
+
+		public string ReadableName { get; }
+	}
+}
