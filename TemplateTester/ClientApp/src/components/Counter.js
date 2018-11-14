ae407e781ce09dd77a54313234d1bfe4b9dac3ca
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-13 19:14:50 -0700

    Expose Decrement action via new button in Counter

--- TemplateTester/ClientApp/src/components/Counter.js
+++ TemplateTester/ClientApp/src/components/Counter.js
@@ -6,8 +6,9 @@ import { actionCreators } from '../store/Counter'
 const Counter = props => (
 	<div>
 		<h1>Counter</h1>
-		<p>This is a simple example of a React component.</p>
 		<p>Current count: <strong>{props.count}</strong></p>
+
+		<button onClick={props.decrement}>Decrement</button>
 		<button onClick={props.increment}>Increment</button>
 	</div>
 )
