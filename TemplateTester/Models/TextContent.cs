7bb0923e75e82720f2e1a6693d33b9a39c4af48f
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-14 18:33:01 -0700

    Introduce TextContent model

---
+++ TemplateTester/Models/TextContent.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace TemplateTester.Models
+{
+	public class TextContent : StringContent
+	{
+		private const string _ContentTypeCharSet = "utf-8";
+		public static MediaTypeHeaderValue TextContentType = new MediaTypeHeaderValue("text/plain") { CharSet = _ContentTypeCharSet };
+
+		public TextContent() : base(string.Empty)
+		{
+			Headers.ContentType = TextContentType;
+		}
+
+		public TextContent(string value) : base(value)
+		{
+			Headers.ContentType = TextContentType;
+		}
+
+		public TextContent(JToken value) : base(value.ToString(Formatting.None))
+		{
+			Headers.ContentType = TextContentType;
+		}
+	}
+}
