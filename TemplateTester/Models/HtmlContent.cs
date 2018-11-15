c8740735fdb5ca9eb890037f71a9d798746169bb
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-14 18:33:14 -0700

    Introduce HtmlContent model

---
+++ TemplateTester/Models/HtmlContent.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace TemplateTester.Models
+{
+	public class HtmlContent : StringContent
+	{
+		private const string _ContentTypeCharSet = "utf-8";
+		public static MediaTypeHeaderValue HtmlContentType = new MediaTypeHeaderValue("text/html") { CharSet = _ContentTypeCharSet };
+
+		public HtmlContent() : base(string.Empty)
+		{
+			Headers.ContentType = HtmlContentType;
+		}
+
+		public HtmlContent(string value) : base(value)
+		{
+			Headers.ContentType = HtmlContentType;
+		}
+
+		public HtmlContent(JToken value) : base(value.ToString(Formatting.None))
+		{
+			Headers.ContentType = HtmlContentType;
+		}
+	}
+}
