f021a8506826d251c86480e829463664ae7d9557
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-14 18:37:38 -0700

    Use new in-memory repository in Startup with dependency injection

--- TemplateTester/Startup.cs
+++ TemplateTester/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TemplateTester.Repositories;
 
 namespace TemplateTester
 {
@@ -21,6 +23,8 @@ namespace TemplateTester
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+			services.TryAddSingleton<IEntityRepository>(new InMemoryEntityRepository());
 
 			// In production, the React files will be served from this directory
 			services.AddSpaStaticFiles(configuration =>
