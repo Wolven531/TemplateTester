7adecf98db7d29847f3775dddc95fa73fbfb441a
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-14 18:34:19 -0700

    Introduce IEntityRepository and in-memory implementation

---
+++ TemplateTester/Repositories/IEntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemplateTester.Models;
+
+namespace TemplateTester.Repositories
+{
+	public interface IEntityRepository
+	{
+		IEnumerable<SimpleEntity> GetAllEntities();
+
+		void AddEntity(SimpleEntity newEntity);
+	}
+}
---
+++ TemplateTester/Repositories/InMemoryEntityRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TemplateTester.Models;
+
+namespace TemplateTester.Repositories
+{
+	public class InMemoryEntityRepository : IEntityRepository
+	{
+		private readonly List<SimpleEntity> _Entities;
+
+		public InMemoryEntityRepository()
+		{
+			_Entities = new List<SimpleEntity>();
+		}
+
+		public void AddEntity(SimpleEntity newEntity)
+		{
+			_Entities.Add(newEntity);
+		}
+
+		public IEnumerable<SimpleEntity> GetAllEntities()
+		{
+			return _Entities;
+		}
+	}
+}
