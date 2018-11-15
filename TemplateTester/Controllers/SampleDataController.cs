c6c9c5a82642e293879d0a540a1489fa127bac6c
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-14 19:03:14 -0700

    Exclude irrelevant classes from code coverage

--- TemplateTester/Controllers/SampleDataController.cs
+++ TemplateTester/Controllers/SampleDataController.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TemplateTester.Controllers
 {
+	[ExcludeFromCodeCoverage]
 	[Route("api/[controller]")]
 	public class SampleDataController : Controller
 	{
--- TemplateTester/Program.cs
+++ TemplateTester/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Logging;
 
 namespace TemplateTester
 {
+	[ExcludeFromCodeCoverage]
 	public class Program
 	{
 		public static void Main(string[] args)
