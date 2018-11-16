8269932492d7ad592b578749d576cb8ab4d076c0
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-15 20:19:09 -0700

    Convert Counter component from stateless to stateful
    * Supports alterByAmount action

--- TemplateTester/ClientApp/src/components/Counter.js
+++ TemplateTester/ClientApp/src/components/Counter.js
@@ -3,15 +3,41 @@ import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { actionCreators } from '../store/Counter'
 
-const Counter = props => (
-	<div>
-		<h1>Counter</h1>
-		<p>Current count: <strong>{props.count}</strong></p>
+class Counter extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			alterAmount: 0
+		}
+	}
 
-		<button onClick={props.decrement}>Decrement</button>
-		<button onClick={props.increment}>Increment</button>
-	</div>
-)
+	render() {
+		return <div>
+			<h1>Counter</h1>
+			<p>Current count: <strong>{this.props.count}</strong></p>
+			<div>
+				<label htmlFor="alterAmount">Alter amount</label>
+				<input
+					id="alterAmount"
+					type="number"
+					step={1}
+					max={100} min={-100}
+					value={this.state.alterAmount}
+					onChange={this.handleChangeAlterAmount} />
+				<button onClick={this.alterByAmount}>Alter by {this.state.alterAmount}</button>
+			</div>
+			<button onClick={this.props.decrement}>Decrement</button>
+			<button onClick={this.props.increment}>Increment</button>
+		</div>
+	}
+
+	alterByAmount = () => this.props.alterByAmount(this.state.alterAmount)
+
+	handleChangeAlterAmount = (changeEvent) => {
+		const alterAmount = parseInt(changeEvent.currentTarget.value, 10)
+		this.setState({ alterAmount })
+	}
+}
 
 export default connect(
 	state => state.counter,
