cbe8f26ccd414f41dca7defcc2e525ad4baa9101
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-20 21:02:45 -0700

    First pass at entity storage UI in React

--- TemplateTester/ClientApp/src/components/Home.js
+++ TemplateTester/ClientApp/src/components/Home.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const Home = props => (
-	<div>
-		<h1>Hello, world!</h1>
-	</div>
-)
+class Home extends React.Component {
+	entities = [
+		{ asdf: 'qwer', someNum: -5.43 }
+	]
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello, world!</h1>
+				<div>
+					<h2>Entity Store</h2>
+					{this.entities.length === 0 && <p>No entities stored</p>}
+					<ul>
+						{this.entities.map((entity, ind) => <li key={`entity-${ind}`}>{JSON.stringify(entity, null, 4)}</li>)}
+					</ul>
+				</div>
+			</div>
+		)
+	}
+}
 
 export default connect()(Home)
