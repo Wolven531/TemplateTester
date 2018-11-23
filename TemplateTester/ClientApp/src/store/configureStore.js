a2e95648fa01ac956986b346275a735416aeba45
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-23 13:11:21 -0700

    Include the Entities reducer in the store

--- TemplateTester/ClientApp/src/store/configureStore.js
+++ TemplateTester/ClientApp/src/store/configureStore.js
@@ -2,11 +2,13 @@
 import thunk from 'redux-thunk'
 import { routerReducer, routerMiddleware } from 'react-router-redux'
 import * as Counter from './Counter'
+import * as Entities from './Entities'
 import * as WeatherForecasts from './WeatherForecasts'
 
 export default function configureStore(history, initialState) {
 	const reducers = {
 		counter: Counter.reducer,
+		entities: Entities.reducer,
 		weatherForecasts: WeatherForecasts.reducer
 	}
 
