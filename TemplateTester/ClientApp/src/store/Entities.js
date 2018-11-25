f719199970a3d7067e09b2c479be064b49175071
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-25 14:58:00 -0700

    Update entities reducer to store entities as separate variable

--- TemplateTester/ClientApp/src/store/Entities.js
+++ TemplateTester/ClientApp/src/store/Entities.js
@@ -1,13 +1,13 @@
 ﻿const ADD_ENTITY = 'add_entity'
 const GET_ALL_ENTITIES = 'get_all_entities'
 
-const initialState = {
-	entities: [
-		{ asdf: 'qwer', someNum: -5.43 },
-		{ id: '70004d80-ee09-4884-a288-9e6b92798ad3', name: 'Shape B', isMaxed: true },
-		{ relatedThings: [{ content: 'thing1' }, { content: 'thing2' } ] }
-	]
-}
+const entities = [
+	{ asdf: 'qwer', someNum: -5.43 },
+	{ id: '70004d80-ee09-4884-a288-9e6b92798ad3', name: 'Shape B', isMaxed: true },
+	{ relatedThings: [{ content: 'thing1' }, { content: 'thing2' } ] }
+]
+
+const initialState = { entities }
 
 const actionCreators = {
 	addEntity: (newEntity) => ({
@@ -21,15 +21,15 @@ const reducer = (state, action) => {
 	state = state || initialState
 
 	const { payload, type } = action
-	const { entities } = state
+	//const { entities } = state
 
 	console.info(`[reducer | entities] type=${type}`)
+	// console.info(`[reducer | entities] state=${JSON.stringify(state, null, 4)}`)
 
 	switch (type) {
 		case ADD_ENTITY:
-			console.info(`[reducer | entities] entities.length=${entities.length} payload.newEntity=${JSON.stringify(payload.newEntity, null, 4)}`)
-			entities.splice(entities.length, 0, payload.newEntity)
-			console.info(`[reducer | entities] entities.length=${entities.length}`)
+			//entities.splice(entities.length, 0, payload.newEntity)
+			entities.push(payload.newEntity)
 			return { ...state, entities }
 		case GET_ALL_ENTITIES:
 			// TODO: is this necessary?
