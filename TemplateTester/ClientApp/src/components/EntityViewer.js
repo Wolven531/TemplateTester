4025462ce3b09d75130f9ba78ca6883a81627b39
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-25 14:58:15 -0700

    Update EntityViewer to be stateless

--- TemplateTester/ClientApp/src/components/EntityViewer.js
+++ TemplateTester/ClientApp/src/components/EntityViewer.js
@@ -1,26 +1,46 @@
-import React from 'react'
-import { bindActionCreators } from 'redux'
+import * as React from 'react'
 import { connect } from 'react-redux'
+// import { bindActionCreators, dispatch } from 'redux'
+
 import { actionCreators } from '../store/Entities'
 
-class EntityViewer extends React.Component {
-	render() {
-		return (
-			<div>
-				<h2>Entity Viewer</h2>
-				{this.props.entities.length === 0
-					? <p>No entities stored</p>
-					: <ul>{this.props.entities.map((entity, ind) =>
-							<li key={`entity-${ind}`}>
-								<pre>{JSON.stringify(entity, null, 4)}</pre>
-							</li>)}
-					</ul>}
-			</div>
-		)
+const EntityViewer = ({ entitiesReducer }) => {
+	const { entities } = entitiesReducer
+
+	return <div>
+		<h2>Entity Viewer</h2>
+		{entities.length === 0
+			? <p>No entities stored</p>
+			: <ul>{entities.map((entity, ind) =>
+					<li key={`entity-${ind}`}>
+						<pre>{JSON.stringify(entity, null, 4)}</pre>
+					</li>)}
+			</ul>}
+	</div>
+}
+
+const mapStateToProps = state => {
+	console.info(`[ mapStateToProps | EntityViewer ]`)
+	// NOTE: pull entities reducer state out of overall state tree
+	const { entities } = state
+
+	return { entitiesReducer: entities }
+}
+
+const mapDispatchToProps = dispatch => {
+	console.info(`[ mapDispatchToProps | EntityViewer ]`)
+	return {
+		addEntity: (newEntity) => {
+			dispatch(actionCreators.addEntity(newEntity))
+		},
+		getAllEntities: () => {
+			dispatch(actionCreators.getAllEntities())
+		}
 	}
 }
 
 export default connect(
-	state => state.entities,
-	dispatch => bindActionCreators(actionCreators, dispatch)
+	mapStateToProps,
+	mapDispatchToProps
+	// dispatch => bindActionCreators(actionCreators, dispatch)
 )(EntityViewer)
