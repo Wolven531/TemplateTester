3c453f63fff2c0caccfeed28e4da8b868809a277
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-24 23:02:40 -0700

    Added EntityAdder component

---
+++ TemplateTester/ClientApp/src/components/EntityAdder.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { actionCreators } from '../store/Entities'
+
+class EntityAdder extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			newEntityText: ''
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Entity Adder</h2>
+				<div>
+					<label htmlFor="newEntityText">New Entity</label>
+					<textarea
+						id="newEntityText"
+						name="newEntityText"
+						value={this.state.newEntityText}
+						onChange={this.handleNewEntityTextChange} />
+				</div>
+				<button onClick={this.handleAddEntityClick}>Add Entity</button>
+			</div>
+		)
+	}
+
+	handleAddEntityClick = (clickEvent) => {
+		this.props.addEntity({ userInput: this.state.newEntityText })
+	}
+
+	handleNewEntityTextChange = (changeEvent) => {
+		if (!changeEvent || !changeEvent.target) {
+			console.warn(
+				'[handleNewEntityTextChange] Cannot continue change event - missing target',
+				changeEvent,
+				changeEvent.target)
+			return
+		}
+		this.setState({
+			newEntityText: changeEvent.target.value
+		})
+	}
+}
+
+export default connect(
+	state => state.entities,
+	dispatch => bindActionCreators(actionCreators, dispatch)
+)(EntityAdder)
