04561b566bf79f1d4659d99739f0ca71cee96ac7
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-25 21:32:08 -0700

    Integrate localStorage API for Entities reducer (persistence)

--- TemplateTester/ClientApp/src/store/Entities.js
+++ TemplateTester/ClientApp/src/store/Entities.js
@@ -1,6 +1,19 @@
 ﻿const ADD_ENTITY = 'add_entity'
 const CLEAR_ALL_ENTITIES = 'clear_all_entities'
 const GET_ALL_ENTITIES = 'get_all_entities'
+const LOAD_ENTITIES_LOCAL = 'load_entities_local'
+const SAVE_ENTITIES_LOCAL = 'save_entities_local'
+
+const LOCAL_STORAGE_KEY_ENTITIES = 'awill.reducers.Entities.entities'
+
+const loadFromLocalStorage = () => {
+	const entitiesString = localStorage.getItem(LOCAL_STORAGE_KEY_ENTITIES)
+	if (!entitiesString) {
+		console.warn('localStorage did not contain stored entities, clearing...')
+		return []
+	}
+	return JSON.parse(entitiesString)
+}
 
 const entities = [
 	{ asdf: 'qwer', someNum: -5.43 },
@@ -16,7 +29,9 @@ const actionCreators = {
 		payload: { newEntity }
 	}),
 	clearAllEntities: () => ({ type: CLEAR_ALL_ENTITIES }),
-	getAllEntities: () => ({ type: GET_ALL_ENTITIES })
+	getAllEntities: () => ({ type: GET_ALL_ENTITIES }),
+	loadEntitiesLocalStorage: () => ({ type: LOAD_ENTITIES_LOCAL }),
+	saveEntitiesLocalStorage: () => ({ type: SAVE_ENTITIES_LOCAL })
 }
 
 const reducer = (state, action) => {
@@ -27,14 +42,18 @@ const reducer = (state, action) => {
 
 	switch (type) {
 		case ADD_ENTITY:
-			//entities.splice(entities.length, 0, payload.newEntity)
-			entities.push(payload.newEntity)
+			entities.splice(entities.length, 0, payload.newEntity)
 			return { ...state, entities }
 		case CLEAR_ALL_ENTITIES:
 			entities.splice(0, entities.length)
 			return { ...state, entities }
 		case GET_ALL_ENTITIES:
+			break;
+		case LOAD_ENTITIES_LOCAL:
+			entities.splice(0, entities.length, ...loadFromLocalStorage())
 			return { ...state, entities }
+		case SAVE_ENTITIES_LOCAL:
+			localStorage.setItem(LOCAL_STORAGE_KEY_ENTITIES, JSON.stringify(entities))
 		default:
 			break;
 	}
