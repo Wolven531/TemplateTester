d5cc4f7c987b2907be1d5229b36badad6526f097
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-26 18:02:53 -0700

    Use Idler reducer in configureStore

--- TemplateTester/ClientApp/src/store/configureStore.js
+++ TemplateTester/ClientApp/src/store/configureStore.js
@@ -1,14 +1,17 @@
 ﻿import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
 import thunk from 'redux-thunk'
 import { routerReducer, routerMiddleware } from 'react-router-redux'
+
 import * as Counter from './Counter'
 import * as Entities from './Entities'
+import * as Idler from './Idler'
 import * as WeatherForecasts from './WeatherForecasts'
 
 export default function configureStore(history, initialState) {
 	const reducers = {
 		counter: Counter.reducer,
 		entities: Entities.reducer,
+		idler: Idler.reducer,
 		weatherForecasts: WeatherForecasts.reducer
 	}
 
