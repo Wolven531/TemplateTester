6a7d704b72fd84ee2d7134a07fd765979e425999
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-27 21:46:08 -0700

    Update Idler component with buy worker button

--- TemplateTester/ClientApp/src/components/Idler.js
+++ TemplateTester/ClientApp/src/components/Idler.js
@@ -2,7 +2,10 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import { actionCreators } from '../store/Idler'
+import {
+	COST_WORKER,
+	actionCreators
+} from '../store/Idler'
 
 class SimpleIdler extends React.Component {
 	componentDidMount() {
@@ -20,10 +23,21 @@ class SimpleIdler extends React.Component {
 					<h2>Workers: {this.props.numWorkers}</h2>
 				</section>
 				<section>
-					<button onClick={this.props.generateResource}>Generate Resource</button>
+					<button onClick={this.props.generateResource}>Generate Resource (+1)</button>
+				</section>
+				<section>
+					<button disabled={this.props.numResource < COST_WORKER}
+						onClick={this.handleBuyWorkerClick}>Buy Worker (Cost={COST_WORKER})</button>
 				</section>
 			</article>
 		)
+	}
+
+	handleBuyWorkerClick = (evt) => {
+		if (this.props.numResource < COST_WORKER) {
+			return
+		}
+		this.props.buyWorker()
 	}
 }
 
