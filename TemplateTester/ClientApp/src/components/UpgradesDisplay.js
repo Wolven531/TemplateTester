4e9f1d562c4653d97a6dd4ff71741fa7dff88971
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-01 13:33:42 -0700

    Introduce UpgradesDisplay

---
+++ TemplateTester/ClientApp/src/components/UpgradesDisplay.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+// import { PRODUCTION_WORKER } from '../store/Idler'
+
+const UpgradesDisplay = ({
+	numResource
+}) => (
+	<section>
+		<h2>Upgrades:</h2>
+	</section>
+)
+
+export { UpgradesDisplay }
