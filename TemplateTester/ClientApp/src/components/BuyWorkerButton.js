381444db22c51f19061d0df65130ba1724e343cd
Anthony Williams <anthony.williams.cs@gmail.com>
2018-11-30 22:57:28 -0700

    Enhance BuyWorkerButton display

--- TemplateTester/ClientApp/src/components/BuyWorkerButton.js
+++ TemplateTester/ClientApp/src/components/BuyWorkerButton.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 
+import './BuyWorkerButton.css'
+
 const BuyWorkerButton = ({
 	buyWorker,
 	cost,
 	disabled,
 	production }) =>
-		<button disabled={disabled}
-			onClick={buyWorker}>Buy Worker ( Cost={cost} Prod={production} )</button>
+		<button className="buy-worker-button" disabled={disabled}
+			onClick={buyWorker}>Buy Worker
+				<em className="negative-number">Cost={cost}</em>
+				<em className="positive-number">Prod={production}</em>
+			</button>
 
 export { BuyWorkerButton }
