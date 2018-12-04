2b72d8bcc1c768ec9f965be16a48e83be3ec269e
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-03 20:02:10 -0700

    Update UpgradesDisplay with basic info

--- TemplateTester/ClientApp/src/components/UpgradesDisplay.js
+++ TemplateTester/ClientApp/src/components/UpgradesDisplay.js
@@ -1,12 +1,25 @@
 import * as React from 'react'
 
-// import { PRODUCTION_WORKER } from '../store/Idler'
+import { Upgrades } from '../models/Upgrades'
 
-const UpgradesDisplay = ({
-	numResource
-}) => (
+import './UpgradesDisplay.css'
+
+const UpgradesDisplay = ({ numResource }) => (
 	<section>
 		<h2>Upgrades:</h2>
+		{Object.getOwnPropertyNames(Upgrades)
+			.map(upgradeKey => {
+				const upgrade = Upgrades[upgradeKey]
+				const { cost, name } = upgrade
+
+				return (
+					<article className="upgrade-container" key={name.replace(' ', '-')}>
+						<h3>{name}</h3>
+						<p>Price: {cost} resources</p>
+					</article>
+				)
+			}
+		)}
 	</section>
 )
 
