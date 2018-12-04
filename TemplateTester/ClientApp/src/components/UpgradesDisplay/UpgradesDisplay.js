8b87001a7b343f255c410e9b79acc374a09d84bc
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-03 20:23:55 -0700

    Enhance upgrade display for clarity

--- TemplateTester/ClientApp/src/components/UpgradesDisplay/UpgradesDisplay.js
+++ TemplateTester/ClientApp/src/components/UpgradesDisplay/UpgradesDisplay.js
@@ -5,18 +5,23 @@ import { Upgrades } from '../../models/Upgrades'
 import './UpgradesDisplay.css'
 
 const UpgradesDisplay = ({ numResource }) => (
-	<section>
+	<section className="upgrades-display">
 		<h2>Upgrades:</h2>
 		{Object.getOwnPropertyNames(Upgrades)
 			.map(upgradeKey => {
 				const upgrade = Upgrades[upgradeKey]
 				const { cost, name } = upgrade
+				const percentage = numResource >= cost ? 100 : (1.0 * numResource / cost * 100).toFixed(2)
 
 				return (
 					<article className="upgrade-container" key={name.replace(' ', '-')}>
 						<h3>{name}</h3>
-						<p>Price: {cost} resources</p>
-						<progress value={numResource % cost} max={cost} />
+						<p>
+						<span className="cost">{cost} resources ({percentage}%)</span>
+							<progress value={percentage} max={100}>
+								{percentage}%
+							</progress>
+						</p>
 					</article>
 				)
 			}
