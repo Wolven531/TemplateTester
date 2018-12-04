7f5ae7355baea2936defa38b71782bf8d67212cb
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-03 20:01:37 -0700

    Add Upgrades model

---
+++ TemplateTester/ClientApp/src/models/Upgrades.js
@@ -0,0 +1,10 @@
+const WORKER_PRODUCTION_1 = 'worker_production_one'
+
+const Upgrades = { }
+
+Upgrades[WORKER_PRODUCTION_1] = {
+	cost: 100,
+	name: 'Gloves for the workers'
+}
+
+export { Upgrades }
