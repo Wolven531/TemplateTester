cde2f0159fd1a01b03e0d2026892b8c8bc9e10bd
Anthony Williams <anthony.williams.cs@gmail.com>
2018-12-08 14:08:17 -0700

    Temporarily comment out unused HtmlContent model class

--- TemplateTester/Models/HtmlContent.cs
+++ TemplateTester/Models/HtmlContent.cs
@@ -5,24 +5,24 @@ using System.Net.Http.Headers;
 
 namespace TemplateTester.Models
 {
-	public class HtmlContent : StringContent
-	{
-		private const string _ContentTypeCharSet = "utf-8";
-		public static MediaTypeHeaderValue HtmlContentType = new MediaTypeHeaderValue("text/html") { CharSet = _ContentTypeCharSet };
+	//public class HtmlContent : StringContent
+	//{
+	//	private const string _ContentTypeCharSet = "utf-8";
+	//	public static MediaTypeHeaderValue HtmlContentType = new MediaTypeHeaderValue("text/html") { CharSet = _ContentTypeCharSet };
 
-		//public HtmlContent() : base(string.Empty)
-		//{
-		//	Headers.ContentType = HtmlContentType;
-		//}
+	//	//public HtmlContent() : base(string.Empty)
+	//	//{
+	//	//	Headers.ContentType = HtmlContentType;
+	//	//}
 
-		public HtmlContent(string value) : base(value)
-		{
-			Headers.ContentType = HtmlContentType;
-		}
+	//	public HtmlContent(string value) : base(value)
+	//	{
+	//		Headers.ContentType = HtmlContentType;
+	//	}
 
-		//public HtmlContent(JToken value) : base(value.ToString(Formatting.None))
-		//{
-		//	Headers.ContentType = HtmlContentType;
-		//}
-	}
+	//	//public HtmlContent(JToken value) : base(value.ToString(Formatting.None))
+	//	//{
+	//	//	Headers.ContentType = HtmlContentType;
+	//	//}
+	//}
 }
