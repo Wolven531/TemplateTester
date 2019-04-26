263815dd70d5dcfa97327a6b0ab7fb184de14251
Anthony Williams <anthony.williams.cs@gmail.com>
2019-04-25 23:45:05 -0600

    Ensure Unit tests are all green

--- TemplateTester/Startup.cs
+++ TemplateTester/Startup.cs
@@ -11,72 +11,72 @@ using TemplateTester.Repositories;
 
 namespace TemplateTester
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			//Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            //Configuration = configuration;
+        }
 
-		//public IConfiguration Configuration { get; }
+        //public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			//services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-			services
-				.AddMvcCore()
-				.AddJsonFormatters()
-				.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services
+                .AddMvcCore()
+                .AddJsonFormatters()
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
 
-			services.TryAddSingleton<IEntityRepository>(new InMemoryEntityRepository());
+            services.TryAddSingleton<IEntityRepository>(new InMemoryEntityRepository());
 
-			// In production, the React files will be served from this directory
-			services.AddSpaStaticFiles(configuration =>
-			{
-				configuration.RootPath = "ClientApp/build";
-			});
-		}
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+        }
 
-		[ExcludeFromCodeCoverage]
-		private void SetupDevelopmentExceptionHandlers(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-		}
+        [ExcludeFromCodeCoverage]
+        private void SetupDevelopmentExceptionHandlers(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			SetupDevelopmentExceptionHandlers(app, env);
-			if (!env.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Error");
-				app.UseHsts();
-			}
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            SetupDevelopmentExceptionHandlers(app, env);
+            if (!env.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-			app.UseSpaStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller}/{action=Index}/{id?}");
-			});
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
 
-			app.UseSpa(spa =>
-			{
-				spa.Options.SourcePath = "ClientApp";
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
 
-				if (env.IsDevelopment())
-				{
-					spa.UseReactDevelopmentServer(npmScript: "start");
-				}
-			});
-		}
-	}
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+        }
+    }
 }
